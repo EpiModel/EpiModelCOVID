095e7fc423d235735e47398b9bfa3ff436f97270
Samuel Jenness <smjenness@gmail.com>
2020-05-17 16:13:43 -0400

    Core package files

---
+++ R/EpiModelCOVID-package.R
@@ -0,0 +1,21 @@
+
+#' Model Extensions to EpiModel for the Network-Based Modeling of COVID-19
+#'
+#' \tabular{ll}{
+#'    Package: \tab EpiModelCOVID\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 1.0.0\cr
+#'    Date: \tab 2020-05-17\cr
+#'    License: \tab GPL-3\cr
+#'    LazyLoad: \tab yes\cr
+#' }
+#'
+#' @name EpiModelCOVID-package
+#' @aliases EpiModelCOVID
+#'
+#' @import EpiModel tergmLite
+#'
+#' @docType package
+#' @keywords package
+#'
+NULL
