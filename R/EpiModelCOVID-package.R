d595cb286b2956ffcf0916a4d23811b547e6f01c
Samuel Jenness <smjenness@gmail.com>
2021-02-18 16:27:09 -0500

    Bump data, R depend

--- R/EpiModelCOVID-package.R
+++ R/EpiModelCOVID-package.R
@@ -39,3 +39,20 @@ NULL
 #' @name moduleset-ship
 #'
 NULL
+
+#' @title EpiModel Module Set for Corporate Office Model
+#'
+#' @description This set of functions is associated with the EpiModel study of
+#'              COVID in corporate office environments.
+#'
+#' @param x An \code{EpiModel} object of class `netest`.
+#' @param param An \code{EpiModel} object of class `param.net`.
+#' @param init An \code{EpiModel} object of class `init.net`.
+#' @param control An \code{EpiModel} object of class `control.net`.
+#' @param s Simulation number, used for restarting dependent simulations.
+#' @param dat Master list object of network models.
+#' @param at Current time step.
+#'
+#' @name moduleset-corporate
+#'
+NULL
