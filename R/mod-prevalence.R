6778137fd6c83b5ebf0e007d88525c776114dead
kkrupins <kathryn.krupinsky@emory.edu>
2021-06-16 20:24:59 -0400

    Adding in tracker for partial immunity (with and without limiting for status)

--- R/mod-prevalence.R
+++ R/mod-prevalence.R
@@ -60,7 +60,7 @@ prevalence_covid_corporate <- function(dat, at) {
 
   # Initialize Outputs
   var.names <- c("num", "s.num", "e.num", "a.num", "ip.num", "ic.num", "r.num",
-                 "h.num", "v1.num", "v2.num")
+                 "h.num", "v1.num", "v1immun.num", "v2.num", "vaxinf.num")
   if (at == 1) {
     for (i in seq_along(var.names)) {
       dat <- add_epi(dat, var.names[i])
@@ -78,7 +78,11 @@ prevalence_covid_corporate <- function(dat, at) {
   dat <- set_epi(dat, "r.num", at, sum(active == 1 & status == "r"))
   dat <- set_epi(dat, "h.num", at, sum(active == 1 & status == "h"))
   dat <- set_epi(dat, "v1.num", at, sum(active == 1 & status == "s" & vax == 1))
+  dat <- set_epi(dat, "v1immun.s.num", at, sum(active == 1 & status == "s" &
+                                               vax == 2))
+  dat <- set_epi(dat, "v1immun.all.num", at, sum(active == 1 & vax == 2))
   dat <- set_epi(dat, "v2.num", at, sum(active == 1 & status == "s" & vax == 3))
+
 
   return(dat)
 }
