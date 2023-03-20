d179857336b043c016ded77f7419c364ad59ed70
chad-klumb <cklumb@gmail.com>
2023-03-20 11:36:33 -0700

    adjust pre-simulation network indexing
    there's no direct precedent for this setup in core EpiModel, but I think using the first resimulated network index minus one is reasonable

--- R/mod-simnet.R
+++ R/mod-simnet.R
@@ -13,7 +13,7 @@ resim_nets_covid_ship <- function(dat, at) {
 
   ## network resimulation
   dat.updates <- NVL(get_control(dat, "dat.updates"), function(dat, ...) dat)
-  dat <- dat.updates(dat = dat, at = at, network = 0L)
+  dat <- dat.updates(dat = dat, at = at, network = nets[1L] - 1L)
   for (network in nets) {
     dat <- simulate_dat(dat = dat, at = at, network = network)
     dat <- dat.updates(dat = dat, at = at, network = network)
