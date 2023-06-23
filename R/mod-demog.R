3e48189cdcc29d805fbef94672d652af5d86c6d1
Maria Garcia Quesada <maria.garcia.quesada@emory.edu>
2023-06-23 16:10:24 -0400

    Delete cruise ship functions

--- R/mod-demog.R
+++ R/mod-demog.R
@@ -27,54 +27,6 @@ aging_covid <- function(dat, at) {
 }
 
 
-#' @rdname moduleset-ship
-#' @export
-deaths_covid_ship <- function(dat, at) {
-
-  ## Attributes ##
-  active <- dat$attr$active
-  age <- dat$attr$age
-  status <- dat$attr$status
-
-  ## Parameters ##
-  mort.rates <- dat$param$mort.rates
-  mort.dis.mult <- dat$param$mort.dis.mult
-
-  idsElig <- which(active == 1)
-  nElig <- length(idsElig)
-  nDeaths <- nDeathsIC <- 0
-
-  if (nElig > 0) {
-
-    whole_ages_of_elig <- pmin(ceiling(age[idsElig]), 86)
-    death_rates_of_elig <- mort.rates[whole_ages_of_elig]
-
-    idsElig.inf <- which(status[idsElig] == "ic")
-    death_rates_of_elig[idsElig.inf] <- death_rates_of_elig[idsElig.inf] * mort.dis.mult
-
-    vecDeaths <- which(rbinom(nElig, 1, death_rates_of_elig) == 1)
-    idsDeaths <- idsElig[vecDeaths]
-    nDeaths <- length(idsDeaths)
-    nDeathsIC <- length(intersect(idsDeaths, idsElig.inf))
-
-    if (nDeaths > 0) {
-      dat$attr$active[idsDeaths] <- 0
-      inactive <- which(dat$attr$active == 0)
-      dat$attr <- deleteAttr(dat$attr, inactive)
-      for (i in seq_along(dat$el)) {
-        dat$el[[i]] <- delete_vertices(dat$el[[i]], inactive)
-      }
-    }
-  }
-
-  ## Summary statistics ##
-  dat$epi$d.flow[at] <- nDeaths
-  dat$epi$d.ic.flow[at] <- nDeathsIC
-
-  return(dat)
-}
-
-
 #' @rdname moduleset-corporate
 #' @export
 deaths_covid_corporate <- function(dat, at) {
@@ -123,52 +75,6 @@ deaths_covid_corporate <- function(dat, at) {
   return(dat)
 }
 
-
-#' @rdname moduleset-ship
-#' @export
-offload_covid_ship <- function(dat, at) {
-
-  ## Attributes ##
-  active <- dat$attr$active
-  status <- dat$attr$status
-  dxStatus <- dat$attr$dxStatus
-  type <- dat$attr$type
-
-  ## Parameters ##
-  exit.rate.pass <- dat$param$exit.rate.pass
-  exit.rate.crew <- dat$param$exit.rate.crew
-
-  exit.elig.status <- dat$param$exit.elig.status
-  require.dx <- dat$param$exit.require.dx
-
-  idsElig <- which(active == 1 & status %in% exit.elig.status)
-  if (require.dx == TRUE) {
-    idsElig <- intersect(idsElig, which(dxStatus == 2))
-  }
-  nElig <- length(idsElig)
-  nExits <- 0
-
-  if (nElig > 0) {
-    exit.rates <- ifelse(type[idsElig] == "p", exit.rate.pass, exit.rate.crew)
-    vecExits <- which(rbinom(nElig, 1, exit.rates) == 1)
-    idsExits <- idsElig[vecExits]
-    nExits <- length(idsExits)
-
-    if (nExits > 0) {
-      active[idsExits] <- 0
-      inactive <- which(active == 0)
-      dat$attr <- deleteAttr(dat$attr, inactive)
-      for (i in seq_along(dat$el)) {
-        dat$el[[i]] <- delete_vertices(dat$el[[i]], inactive)
-      }
-    }
-  }
-
-  ## Summary statistics ##
-  dat$epi$exit.flow[at] <- nExits
-
-  return(dat)
-}
 
 #' @rdname moduleset-corporate
 #' @export
