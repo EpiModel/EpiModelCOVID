f7d59cb48bec4ea2604165dd328cac727776bee3
billydehaochen <45851948+billydehaochen@users.noreply.github.com>
2025-11-05 13:25:55 -0500

    push

---
+++ R/default_attributes.R
@@ -0,0 +1,213 @@
+#' Get the list the attributes used by the model with their default value
+#'
+#' @inheritParams aging_msm
+#'
+#' @details
+#' This list must be exhaustive. All attributes should get a default value (even
+#' NA) here.
+#'
+#' @return A named list of all the attributes with default values.
+#'
+#' TODO: document all the attributes
+get_default_attrs <- function(dat) {
+  list(
+    deg_work=0,
+    deg_school=0,
+    deg_nonhome=0
+    # status = 0,
+    # age = get_param(dat, "arrival.age"),
+    # sqrt.age = NA,
+    # active.sex = 1,
+    # deg.casl = 0,
+    # deg.main = 0,
+    # deg.tot = 0,
+    # diag.status = 0,
+    # inf.time = NA,
+    # stage = NA,
+    # stage.time = NA,
+    # aids.time = NA,
+    # diag.stage = NA,
+    # vl = NA,
+    # vl.last.usupp = NA,
+    # vl.last.supp = NA,
+    # diag.time = NA,
+    # tx.status = NA,
+    # cuml.time.on.tx = NA,
+    # cuml.time.off.tx = NA,
+    # tx.init.time = NA,
+    # part.tx.init.time = NA,
+    # part.tx.reinit.time = NA,
+    # ## Gonorrhea
+    # gono.rect = 0,
+    # gono.uret = 0,
+    # gono.rect.sympt = 0,
+    # gono.uret.sympt = 0,
+    # gono.rect.inf.last = -Inf,
+    # gono.uret.inf.last = -Inf,
+    # gono.rect.inf.count = 0,
+    # gono.uret.inf.count = 0,
+    # gono.test.last = -Inf,
+    # gono.dx = 0,
+    # gono.dx.last = 0,
+    # gono.tx = 0,
+    # ## Chlamidya
+    # chla.rect = 0,
+    # chla.uret = 0,
+    # chla.rect.sympt = 0,
+    # chla.uret.sympt = 0,
+    # chla.rect.inf.last = -Inf,
+    # chla.uret.inf.last = -Inf,
+    # chla.rect.inf.count = 0,
+    # chla.uret.inf.count = 0,
+    # chla.test.last = -Inf,
+    # chla.dx = 0,
+    # chla.dx.last = -Inf,
+    # chla.tx = 0,
+    # ## Syphilis
+    # syph.inf = 0,
+    # syph.sympt = 0,
+    # syph.inf.last = -Inf,
+    # syph.inf.count = 0,
+    # syph.test.last = -Inf,
+    # syph.dx = 0,
+    # syph.dx.last = -Inf,
+    # syph.tx = 0,
+    # syph.stage = NA,
+    # syph.stage.last = -Inf,
+    # ##
+    # prep = 0,
+    # prep.start.last = -Inf,
+    # prep.start.count = 0,
+    # prep.indic = 0,
+    # prep.risk.last = -Inf,
+    # act.last = -Inf,
+    # uai.last = -Inf,
+    # usupp.partner.last = -Inf,
+    # unknown.partner.last = -Inf,
+    # #
+    # # computed upon arrival
+    # race = 0,
+    # risk.grp = 0,
+    # late.tester = 0,
+    # circ = 0,
+    # age.grp = 0,
+    # ins.quot = 0,
+    # tt.traj = 0,
+    # last.neg.test = -Inf, # entrTime
+    # role.class = 0,
+    # prep.class = 0
+  )
+}
+
+#' Update the attributes requiring computation for new nodes
+#'
+#' @inheritParams aging_msm
+#' @param n_new The number of new nodes to update
+#' @param post_init logical flag, TRUE if not called by the initialization
+#' module.
+#'
+#' @details
+#' This function takes care of all the attributes requiring some computations.
+#' This includes the random assignment for race or the calculation of the
+#' age groups. New attributes that need special assignment should be set here
+#' AS WELL AS in the `get_default_attrs` function.
+#'
+#' @return
+#' This function returns the `dat` object with updated attributes for the new
+#' nodes.
+make_computed_attrs <- function(dat, n_new, post_init) {
+  new_nodes_pid <- length(get_attr(dat, "active")) - n_new + seq_len(n_new)
+  ns <- get_param(dat, "netstats")
+  age_breaks <- ns$demog$age.breaks
+  race_dist <- prop.table(table(ns$attr$race))
+  race_lvls <- as.numeric(names(race_dist))
+  
+  n_attr <- list()
+  if (post_init) {
+    n_attr$race <- sample(race_lvls, n_new, TRUE, race_dist)
+    n_attr$role.class <- make_role_class(dat, n_attr$race, race_lvls)
+    n_attr$risk.grp <- sample(seq_len(5), n_new, TRUE)
+  } else {
+    n_attr$race <- get_attr(dat, "race", posit_ids = new_nodes_pid)
+    n_attr$role.class <- get_attr(dat, "role.class", posit_ids = new_nodes_pid)
+    n_attr$risk.grp <- get_attr(dat, "risk.grp", posit_ids = new_nodes_pid)
+  }
+  
+  age <- get_attr(dat, "age", posit_ids = new_nodes_pid)
+  
+  n_attr <- c(n_attr, list(
+    late.tester = make_late_tester(dat, n_attr$race),
+    circ        = make_circ(dat, n_attr$race, race_lvls),
+    age.grp     = cut(age, age_breaks, labels = FALSE, right = FALSE),
+    ins.quot    = make_ins_quot(n_attr$role.class),
+    tt.traj     = make_tt_traj(dat, n_attr$race, race_lvls),
+    prep.class   = make_prep_class(dat, length(n_attr$race)),
+    last.neg.test = get_attr(dat, "entrTime", posit_ids = new_nodes_pid)
+  ))
+  
+  for (attr_name in names(n_attr)) {
+    dat <- set_attr(dat, attr_name, n_attr[[attr_name]], posit_ids = new_nodes_pid)
+  }
+  
+  return(dat)
+}
+
+# Generate the `late.tester` attributes
+make_late_tester <- function(dat, race) {
+  rates <- get_param(dat, "hiv.test.late.prob")[race]
+  runif(length(rates)) < rates
+}
+
+# Generate the `ins.quot` attributes
+make_ins_quot <- function(role.class) {
+  ins_quot <- numeric(length(role.class))
+  ins_quot[role.class == 0]  <- 1
+  ins_quot[role.class == 1]  <- 0
+  ins_quot[role.class == 2]  <- runif(sum(role.class == 2))
+  return(ins_quot)
+}
+
+# Generate the `tt.traj` attributes
+make_tt_traj <- function(dat, race, race_lvls) {
+  partial <- get_param(dat, "tt.partial.supp.prob")
+  full    <- get_param(dat, "tt.full.supp.prob")
+  durable <- get_param(dat, "tt.durable.supp.prob")
+  race.new <- race
+  tt_traj <- numeric(length(race.new))
+  for (r in race_lvls) {
+    ids.race <- which(race == r)
+    tt_traj[ids.race] <- sample(
+      seq_len(3), length(ids.race), TRUE,
+      c(partial[r], full[r], durable[r])
+    )
+  }
+  return(tt_traj)
+}
+
+# Generate the `circ` attributes
+make_circ <- function(dat, race, race_lvls) {
+  hiv.circ.prob <- get_param(dat, "hiv.circ.prob")
+  circ <- numeric(length(race))
+  for (r in race_lvls) {
+    ids.race <- which(race == r)
+    circ[ids.race] <- runif(length(ids.race)) < hiv.circ.prob[r]
+  }
+  return(circ)
+}
+
+# Generate the `role.class` attributes
+make_role_class <- function(dat, race, race_lvls) {
+  ns <- get_param(dat, "netstats")$attr
+  role_class <- numeric(length(race))
+  for (r in race_lvls) {
+    ids.race <- which(race == r)
+    rc.probs <- prop.table(table(ns$role.class[ns$race == r]))
+    role_class[ids.race] <- sample(0:2, length(ids.race), TRUE, rc.probs)
+  }
+  return(role_class)
+}
+
+make_prep_class <- function(dat, n_new) {
+  prep.class <- sample(3, n_new, TRUE, get_param(dat, "prep.adhr.dist"))
+  return(prep.class)
+}
--- R/mod-init.R
+++ R/mod-init.R
@@ -1,212 +1,177 @@
 
-
-#' @rdname moduleset-corporate
-#' @export
-init_covid_corporate <- function(x, param, init, control, s) {
-  
-  ## Master Data List Setup ##
-  dat <- create_dat_object(param, init, control)
-  
-  dat <- init_nets(dat, x)
-  
-  # simulate first time step
-  dat <- sim_nets_t1(dat)
-  dat <- summary_nets(dat, at = 1L)
-  
-  # Add household network edgelist
-  dat$num.nw <- dat$num.nw + 1
-  dat$el[[dat$num.nw]] <- as.matrix(dat$param$hh.pairs)
-  attr(dat$el[[dat$num.nw]], 'n') <- get_epi(dat, "sim.num", at = 1)
-  dat$net_attr[[dat$num.nw]] <- list(n = get_epi(dat, "sim.num", at = 1))
-  dat$control[["tergmLite.track.duration"]][[dat$num.nw]] <- FALSE
-  
-  ## Infection Status and Time Modules
-  dat <- init_status_covid_corporate(dat)
-  
-  ## Get initial prevalence
-  dat <- prevalence_covid_corporate(dat, at = 1)
-  
-  return(dat)
-}
-
-
-init_status_covid_corporate <- function(dat) {
-  
-  e.num <- get_init(dat, "e.num")
-  
-  active <- get_attr(dat, "active")
-  num <- sum(active)
-  
-  ## Disease status
-  status <- rep("s", num)
-  if (e.num > 0) {
-    status[sample(which(active == 1), size = e.num)] <- "e"
-  }
-  
-  dat <- set_attr(dat, "status", status)
-  
-  # Age group for vaccination processes
-  age <- get_attr(dat, "age")
-  
-  vax.age.group <- rep(NA, length(age))
-  vax.age.group[age < 5] <- 1
-  vax.age.group[age >= 5 & age < 18] <- 2
-  vax.age.group[age >= 18 & age < 50] <- 3
-  vax.age.group[age >= 50 & age < 65] <- 4
-  vax.age.group[age >= 65] <- 5
-  
-  dat <- set_attr(dat, "vax.age.group", vax.age.group)
-  
-  # Infection Time and related attributes
-  idsInf <- which(status == "e")
-  infTime <- rep(NA, num)
-  clinical <- rep(NA, num)
-  hospit <- rep(NA, num)
-  statusTime <- rep(NA, num)
-  statusTime[idsInf] <- 1
-  dxStatus <- rep(0, num)
-  dxTime <- rep(NA, num)
-  vax <- rep(0, num)
-  vax1Time <- rep(NA, num)
-  vax2Time <- rep(NA, num)
-  vax3Time <- rep(NA, num)
-  vax4Time <- rep(NA, num)
-  isolate <- rep(NA, num)
-  isoTime <- rep(NA, num)
-  
-  dat <- set_attr(dat, "statusTime", statusTime)
-  dat <- set_attr(dat, "infTime", infTime)
-  dat <- set_attr(dat, "clinical", clinical)
-  dat <- set_attr(dat, "hospit", hospit)
-  dat <- set_attr(dat, "dxStatus", dxStatus)
-  dat <- set_attr(dat, "dxTime", dxTime)
-  dat <- set_attr(dat, "vax", vax)
-  dat <- set_attr(dat, "vax1Time", vax1Time)
-  dat <- set_attr(dat, "vax2Time", vax2Time)
-  dat <- set_attr(dat, "vax3Time", vax3Time)
-  dat <- set_attr(dat, "vax4Time", vax4Time)
-  dat <- set_attr(dat, "isolate", isolate)
-  dat <- set_attr(dat, "isoTime", isoTime)
-  
-  return(dat)
-}
-
+source("~/Documents/GitHub/EpiModelCOVID/R/default_attributes.R")
 
 #' @rdname moduleset-gmc19
 #' @export
 init_gmc19 <- function(x, param, init, control, s) {
 
+  ## Master Data List Setup ##
   dat <- create_dat_object(param, init, control)
 
-  dat <- init_nets(dat, x)
-
-
-  # define initial nodal attributes, added as we need this for the x-layer sim
-  node_age_grp <- as.character(init$attr$node.age.grp)
-  node_age     <- as.numeric  (init$attr$node.age)
-  deg_work   <- as.integer  (init$attr$contact_attribute_Work)
-  deg_school <- as.integer  (init$attr$contact_attribute_School)
-  deg_nonhome   <- as.integer  (init$attr$contact_attribute_Nonhome)
-
-  no_contact <- 1L - deg_nonhome
+  ## network and stats initialization
+  dat <- init_nets(dat, x) # this introduce dat$run$attr$deg.x_layer from school/work, which seems not contained in the arguments
   
+  ## Initialize all remaining attributes
+  dat <- init_attrs(dat)
+  dat <- overwrite_attrs(dat)
   
-  ## set attributes for age and contact
-  dat <- set_attr(dat, "age.grp", node_age_grp)
-  dat <- set_attr(dat, "age",     node_age)
-  dat <- set_attr(dat, "deg_school", deg_school)
-  dat <- set_attr(dat, "deg_work",   deg_work)
-  dat <- set_attr(dat, "no.contact",         no_contact)
-
-  # simulate first time step, time 1 to 2
+  # simulate first time step
   dat <- sim_nets_t1(dat)
   dat <- summary_nets(dat, at = 1L)
-
-  # ## add home layer edgelist
-   dat$num.nw <- dat$num.nw + 1L
-   dat$el[[dat$num.nw]] <- as.matrix(dat$param$hh.pairs) # design-time containers
-   attr(dat$el[[dat$num.nw]], 'n') <-  dat$run$num # muted code doesn't work- get_epi(dat, "sim.num", at = 1)
-   dat$net_attr[[dat$num.nw]] <- list(n = dat$run$num) # muted code doesn't work - list(n = get_epi(dat, "sim.num", at = 1))
-   dat$control[["tergmLite.track.duration"]][[dat$num.nw]] <- FALSE
-   
   
-  # add hh_id attribute to each node, load from run from data
-  #dat$attr$hh.ids <-  dat$attr <- list()
-  #dat$attr$hh.ids <-  dat$run$attr$hh.ids
+  num <- sum(get_attr(dat, "active") == 1)
   
-  # code for tergmLite. This is needed because home layer is added after the initiation is completed
-  # this is necessary to make the  depart_nodes work 
-  dat$run$el[[dat$num.nw]]        <- as.matrix(dat$param$hh.pairs)  
-  dat$run$net_attr[[dat$num.nw]]  <- dat$net_attr[[dat$num.nw]]
-
-
-  ## Infection Status and Time Modules
-  dat <- init_status_covid_corporate(dat)
-
-  ## Get initial prevalence
-  dat <- prevalence_covid_corporate(dat, at = 1)
-
+  # Time Unit
+  time.unit <- param$epistats$time.unit
+  dat <- set_param(dat, "time.unit", time.unit)
+  
+  dat[["temp"]] <- list()
+  
+  # Prevalence Tracking
+  dat <- set_epi(dat, "num", at = 1,  num)
+  
   return(dat)
-}
 
 
-init_status_gmc19 <- function(dat) {
+#   # define initial nodal attributes, added as we need this for the x-layer sim
+#   node_age_grp <- as.character(init$attr$node.age.grp)
+#   node_age     <- as.numeric  (init$attr$node.age)
+#   deg_work   <- as.integer  (init$attr$contact_attribute_Work)
+#   deg_school <- as.integer  (init$attr$contact_attribute_School)
+#   deg_nonhome   <- as.integer  (init$attr$contact_attribute_Nonhome)
+# 
+#   no_contact <- 1L - deg_nonhome
+#   
+#   
+#   ## set attributes for age and contact
+#   dat <- set_attr(dat, "age.grp", node_age_grp)
+#   dat <- set_attr(dat, "age",     node_age)
+#   dat <- set_attr(dat, "deg_school", deg_school)
+#   dat <- set_attr(dat, "deg_work",   deg_work)
+#   dat <- set_attr(dat, "no.contact",         no_contact)
+# 
+#   # simulate first time step, time 1 to 2
+#   dat <- sim_nets_t1(dat)
+#   dat <- summary_nets(dat, at = 1L)
+# 
+#   # ## add home layer edgelist
+#    dat$num.nw <- dat$num.nw + 1L
+#    dat$el[[dat$num.nw]] <- as.matrix(dat$param$hh.pairs) # design-time containers
+#    attr(dat$el[[dat$num.nw]], 'n') <-  dat$run$num # muted code doesn't work- get_epi(dat, "sim.num", at = 1)
+#    dat$net_attr[[dat$num.nw]] <- list(n = dat$run$num) # muted code doesn't work - list(n = get_epi(dat, "sim.num", at = 1))
+#    dat$control[["tergmLite.track.duration"]][[dat$num.nw]] <- FALSE
+#    
+#   
+#   # add hh_id attribute to each node, load from run from data
+#   #dat$attr$hh.ids <-  dat$attr <- list()
+#   #dat$attr$hh.ids <-  dat$run$attr$hh.ids
+#   
+#   # code for tergmLite. This is needed because home layer is added after the initiation is completed
+#   # this is necessary to make the  depart_nodes work 
+#   dat$run$el[[dat$num.nw]]        <- as.matrix(dat$param$hh.pairs)  
+#   dat$run$net_attr[[dat$num.nw]]  <- dat$net_attr[[dat$num.nw]]
+# 
+# 
+#   ## Infection Status and Time Modules
+#   dat <- init_status_covid_corporate(dat)
+# 
+#   ## Get initial prevalence
+#   dat <- prevalence_covid_corporate(dat, at = 1)
+# 
+#   return(dat)
+ }
+
+
+init_attrs <- function(dat) { #no.contact needs to be defined here.
   
-  e.num <- get_init(dat, "e.num")
+
   
-  active <- get_attr(dat, "active")
-  num <- sum(active)
+  n_nodes <- sum(get_attr(dat, "active") ==  1)
   
-  ## Disease status
-  status <- rep("s", num)
-  if (e.num > 0) {
-    status[sample(which(active == 1), size = e.num)] <- "e"
+  # add no.contact
+  deg_nonhome   <- get_init(dat, "contact_attribute_Nonhome")
+  no_contact <- 1L - deg_nonhome
+  dat <- set_attr(dat, "no.contact",         no_contact)
+  
+  def_attrs <- get_default_attrs(dat)
+  cur_attrs <- get_attr_list(dat)
+  missing <- names(def_attrs)[!names(def_attrs) %in% names(cur_attrs)]
+  for (attr_name in missing) {
+    dat <- append_attr(dat, attr_name, def_attrs[[attr_name]], n_nodes)
   }
   
-  dat <- set_attr(dat, "status", status)
+  #dat <- make_computed_attrs(dat, n_nodes, post_init = FALSE) # mute for now, age need to be updated
   
-  # Age group for vaccination processes
-  age <- get_attr(dat, "age")
-  
-  vax.age.group <- rep(NA, length(age))
-  vax.age.group[age < 5] <- 1
-  vax.age.group[age >= 5 & age < 18] <- 2
-  vax.age.group[age >= 18 & age < 50] <- 3
-  vax.age.group[age >= 50 & age < 65] <- 4
-  vax.age.group[age >= 65] <- 5
-  
-  dat <- set_attr(dat, "vax.age.group", vax.age.group)
-  
-  # Infection Time and related attributes
-  idsInf <- which(status == "e")
-  infTime <- rep(NA, num)
-  clinical <- rep(NA, num)
-  hospit <- rep(NA, num)
-  statusTime <- rep(NA, num)
-  statusTime[idsInf] <- 1
-  dxStatus <- rep(0, num)
-  dxTime <- rep(NA, num)
-  vax <- rep(0, num)
-  vax1Time <- rep(NA, num)
-  vax2Time <- rep(NA, num)
-  vax3Time <- rep(NA, num)
-  vax4Time <- rep(NA, num)
-  isolate <- rep(NA, num)
-  isoTime <- rep(NA, num)
-  
-  dat <- set_attr(dat, "statusTime", statusTime)
-  dat <- set_attr(dat, "infTime", infTime)
-  dat <- set_attr(dat, "clinical", clinical)
-  dat <- set_attr(dat, "hospit", hospit)
-  dat <- set_attr(dat, "dxStatus", dxStatus)
-  dat <- set_attr(dat, "dxTime", dxTime)
-  dat <- set_attr(dat, "vax", vax)
-  dat <- set_attr(dat, "vax1Time", vax1Time)
-  dat <- set_attr(dat, "vax2Time", vax2Time)
-  dat <- set_attr(dat, "vax3Time", vax3Time)
-  dat <- set_attr(dat, "vax4Time", vax4Time)
-  dat <- set_attr(dat, "isolate", isolate)
-  dat <- set_attr(dat, "isoTime", isoTime)
+  # # HIV-related attributes
+  # dat <- init_status_msm(dat)
+  # 
+  # # STI Status
+  # dat <- gono_chla_init(dat, "gono")
+  # dat <- gono_chla_init(dat, "chla")
+  # dat <- syph_init(dat)
   
   return(dat)
 }
+
+
+# init_status_gmc19 <- function(dat) {
+#   
+#   e.num <- get_init(dat, "e.num")
+#   
+#   active <- get_attr(dat, "active")
+#   num <- sum(active)
+#   
+#   ## Disease status
+#   status <- rep("s", num)
+#   if (e.num > 0) {
+#     status[sample(which(active == 1), size = e.num)] <- "e"
+#   }
+#   
+#   dat <- set_attr(dat, "status", status)
+#   
+#   # Age group for vaccination processes
+#   age <- get_attr(dat, "age")
+#   
+#   vax.age.group <- rep(NA, length(age))
+#   vax.age.group[age < 5] <- 1
+#   vax.age.group[age >= 5 & age < 18] <- 2
+#   vax.age.group[age >= 18 & age < 50] <- 3
+#   vax.age.group[age >= 50 & age < 65] <- 4
+#   vax.age.group[age >= 65] <- 5
+#   
+#   dat <- set_attr(dat, "vax.age.group", vax.age.group)
+#   
+#   # Infection Time and related attributes
+#   idsInf <- which(status == "e")
+#   infTime <- rep(NA, num)
+#   clinical <- rep(NA, num)
+#   hospit <- rep(NA, num)
+#   statusTime <- rep(NA, num)
+#   statusTime[idsInf] <- 1
+#   dxStatus <- rep(0, num)
+#   dxTime <- rep(NA, num)
+#   vax <- rep(0, num)
+#   vax1Time <- rep(NA, num)
+#   vax2Time <- rep(NA, num)
+#   vax3Time <- rep(NA, num)
+#   vax4Time <- rep(NA, num)
+#   isolate <- rep(NA, num)
+#   isoTime <- rep(NA, num)
+#   
+#   dat <- set_attr(dat, "statusTime", statusTime)
+#   dat <- set_attr(dat, "infTime", infTime)
+#   dat <- set_attr(dat, "clinical", clinical)
+#   dat <- set_attr(dat, "hospit", hospit)
+#   dat <- set_attr(dat, "dxStatus", dxStatus)
+#   dat <- set_attr(dat, "dxTime", dxTime)
+#   dat <- set_attr(dat, "vax", vax)
+#   dat <- set_attr(dat, "vax1Time", vax1Time)
+#   dat <- set_attr(dat, "vax2Time", vax2Time)
+#   dat <- set_attr(dat, "vax3Time", vax3Time)
+#   dat <- set_attr(dat, "vax4Time", vax4Time)
+#   dat <- set_attr(dat, "isolate", isolate)
+#   dat <- set_attr(dat, "isoTime", isoTime)
+#   
+#   return(dat)
+# }
+  
